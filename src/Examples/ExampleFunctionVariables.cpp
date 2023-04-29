a23a83f0fac78e490e72bd9f0a7d8ffd28dc40e4
Luke Kasz <me@lukekasz.com>
2023-04-29 08:11:06 +0200

    Removed unused collection of function symbols in ExampleFunctionVariables

--- src/Examples/ExampleFunctionVariables.cpp
+++ src/Examples/ExampleFunctionVariables.cpp
@@ -61,31 +61,23 @@ void ExampleFunctionVariables(const PDB::RawFile& rawPdbFile, const PDB::DBIStre
 	const PDB::ImageSectionStream imageSectionStream = dbiStream.CreateImageSectionStream(rawPdbFile);
 	sectionScope.Done();
 
-
 	// prepare the module info stream for grabbing function symbols from modules
 	TimedScope moduleScope("Reading module info stream");
 	const PDB::ModuleInfoStream moduleInfoStream = dbiStream.CreateModuleInfoStream(rawPdbFile);
 	moduleScope.Done();
-
 
 	// prepare symbol record stream needed by the public stream
 	TimedScope symbolStreamScope("Reading symbol record stream");
 	const PDB::CoalescedMSFStream symbolRecordStream = dbiStream.CreateSymbolRecordStream(rawPdbFile);
 	symbolStreamScope.Done();
 
-	// note that we only use unordered_set in order to keep the example code easy to understand.
-	// using other hash set implementations like e.g. abseil's Swiss Tables (https://abseil.io/about/design/swisstables) is *much* faster.
-	std::vector<FunctionSymbol> functionSymbols;
-	std::unordered_set<uint32_t> seenFunctionRVAs;
-
-	// start by reading the module stream, grabbing every function symbol we can find.
-	// in most cases, this gives us ~90% of all function symbols already, along with their size.
 	{
-		TimedScope scope("Storing function symbols from modules");
+		TimedScope scope("Printing function variable records from modules\n");
 
 		const PDB::ArrayView<PDB::ModuleInfoStream::Module> modules = moduleInfoStream.GetModules();
 
 		uint32_t blockLevel = 0;
+		uint32_t recordCount = 0;
 
 		for (const PDB::ModuleInfoStream::Module& module : modules)
 		{
@@ -95,21 +87,21 @@ void ExampleFunctionVariables(const PDB::RawFile& rawPdbFile, const PDB::DBIStre
 			}
 
 			const PDB::ModuleSymbolStream moduleSymbolStream = module.CreateSymbolStream(rawPdbFile);
-			moduleSymbolStream.ForEachSymbol([&tpiStream, &functionSymbols, &seenFunctionRVAs, &imageSectionStream, &blockLevel](const PDB::CodeView::DBI::Record* record)
+			moduleSymbolStream.ForEachSymbol([&tpiStream, &imageSectionStream, &blockLevel, &recordCount](const PDB::CodeView::DBI::Record* record)
 			{
 				const SymbolRecordKind kind = record->header.kind;
 				const PDB::CodeView::DBI::Record::Data& data = record->data;
 
-				// only grab function symbols from the module streams
-				const char* name = nullptr;
-				uint32_t rva = 0u;
-				uint32_t size = 0u;
-
 				if (record->header.kind == PDB::CodeView::DBI::SymbolRecordKind::S_END)
 				{
-					PDB_ASSERT(blockLevel > 0, "BlockIndent for S_END is 0");
+					PDB_ASSERT(blockLevel > 0, "Block level for S_END is 0");
 					blockLevel--;
 					Printf(blockLevel, "S_END\n");
+
+					if (blockLevel == 0)
+					{
+						Printf(0, "\n");
+					}
 				}
 				else if (record->header.kind == PDB::CodeView::DBI::SymbolRecordKind::S_BLOCK32)
 				{
@@ -200,7 +192,6 @@ void ExampleFunctionVariables(const PDB::RawFile& rawPdbFile, const PDB::DBIStre
 						Printf(blockLevel, "S_LTHREAD32: '%s' -> '%s'\n", data.S_LTHREAD32.name, typeName.c_str());
 					}
 				}
-
 				else if (record->header.kind == PDB::CodeView::DBI::SymbolRecordKind::S_UDT)
 				{
 					const std::string typeName = GetVariableTypeName(tpiStream, data.S_UDT.typeIndex);
@@ -229,204 +220,74 @@ void ExampleFunctionVariables(const PDB::RawFile& rawPdbFile, const PDB::DBIStre
 				}
 				else if (record->header.kind == PDB::CodeView::DBI::SymbolRecordKind::S_FRAMEPROC)
 				{
-					functionSymbols[functionSymbols.size() - 1].frameProc = record;
-					return;
+					Printf(blockLevel, "S_FRAMEPROC: <TODO>\n");
 				}
 				else if (record->header.kind == PDB::CodeView::DBI::SymbolRecordKind::S_THUNK32)
 				{
+					PDB_ASSERT(blockLevel == 0, "BlockLevel %u != 0", blockLevel);
+
 					if (data.S_THUNK32.thunk == PDB::CodeView::DBI::ThunkOrdinal::TrampolineIncremental)
 					{
 						// we have never seen incremental linking thunks stored inside a S_THUNK32 symbol, but better safe than sorry
-						name = "ILT - Thunk";
-						rva = imageSectionStream.ConvertSectionOffsetToRVA(data.S_THUNK32.section, data.S_THUNK32.offset);
-						size = 5u;
+						const uint32_t rva = imageSectionStream.ConvertSectionOffsetToRVA(data.S_THUNK32.section, data.S_THUNK32.offset);
+						Printf(blockLevel, "Function: 'ILT/Thunk' | RVA 0x%X\n", rva);;
 					}
 					else
 					{
-						name = data.S_THUNK32.name;
-						rva = imageSectionStream.ConvertSectionOffsetToRVA(data.S_THUNK32.section, data.S_THUNK32.offset);
-						size = data.S_THUNK32.length; // Correct ?
+						const uint32_t rva = imageSectionStream.ConvertSectionOffsetToRVA(data.S_THUNK32.section, data.S_THUNK32.offset);
+						Printf(blockLevel, "S_THUNK32 Function '%s' | RVA 0x%X\n", data.S_THUNK32.name, rva);
+						blockLevel++;
 					}
 				}
 				else if (record->header.kind == PDB::CodeView::DBI::SymbolRecordKind::S_TRAMPOLINE)
 				{
+					PDB_ASSERT(blockLevel == 0, "BlockLevel %u != 0", blockLevel);
 					// incremental linking thunks are stored in the linker module
-					name = "ILT - Trampoline";
-					rva = imageSectionStream.ConvertSectionOffsetToRVA(data.S_TRAMPOLINE.thunkSection, data.S_TRAMPOLINE.thunkOffset);
-					size = 5u;
+					const uint32_t rva = imageSectionStream.ConvertSectionOffsetToRVA(data.S_TRAMPOLINE.thunkSection, data.S_TRAMPOLINE.thunkOffset);
+					Printf(blockLevel, "Function 'ILT/Trampoline' | RVA 0x%X\n", rva);
 				}
 				else if (record->header.kind == PDB::CodeView::DBI::SymbolRecordKind::S_LPROC32)
 				{
-					name = data.S_LPROC32.name;
-					rva = imageSectionStream.ConvertSectionOffsetToRVA(data.S_LPROC32.section, data.S_LPROC32.offset);
-					size = data.S_LPROC32.codeSize;
+					PDB_ASSERT(blockLevel == 0, "BlockLevel %u != 0", blockLevel);
+					const uint32_t rva = imageSectionStream.ConvertSectionOffsetToRVA(data.S_LPROC32.section, data.S_LPROC32.offset);
+					Printf(blockLevel, "S_LPROC32 Function '%s' | RVA 0x%X\n", data.S_LPROC32.name, rva);
+					blockLevel++;
 				}
 				else if (record->header.kind == PDB::CodeView::DBI::SymbolRecordKind::S_GPROC32)
 				{
-					name = data.S_GPROC32.name;
-					rva = imageSectionStream.ConvertSectionOffsetToRVA(data.S_GPROC32.section, data.S_GPROC32.offset);
-					size = data.S_GPROC32.codeSize;
+					PDB_ASSERT(blockLevel == 0, "BlockLevel %u != 0", blockLevel);
+					const uint32_t rva = imageSectionStream.ConvertSectionOffsetToRVA(data.S_GPROC32.section, data.S_GPROC32.offset);
+					Printf(blockLevel, "S_GPROC32 Function '%s' | RVA 0x%X\n", data.S_GPROC32.name, rva);
+					blockLevel++;
 				}
 				else if (record->header.kind == PDB::CodeView::DBI::SymbolRecordKind::S_LPROC32_ID)
 				{
-					name = data.S_LPROC32_ID.name;
-					rva = imageSectionStream.ConvertSectionOffsetToRVA(data.S_LPROC32_ID.section, data.S_LPROC32_ID.offset);
-					size = data.S_LPROC32_ID.codeSize;
+					PDB_ASSERT(blockLevel == 0, "BlockLevel %u != 0", blockLevel);
+					const uint32_t rva = imageSectionStream.ConvertSectionOffsetToRVA(data.S_LPROC32_ID.section, data.S_LPROC32_ID.offset);
+					Printf(blockLevel, "S_LPROC32_ID Function '%s' | RVA 0x%X\n", data.S_LPROC32_ID.name, rva);
+					blockLevel++;
 				}
 				else if (record->header.kind == PDB::CodeView::DBI::SymbolRecordKind::S_GPROC32_ID)
 				{
-					name = data.S_GPROC32_ID.name;
-					rva = imageSectionStream.ConvertSectionOffsetToRVA(data.S_GPROC32_ID.section, data.S_GPROC32_ID.offset);
-					size = data.S_GPROC32_ID.codeSize;
+					PDB_ASSERT(blockLevel == 0, "BlockLevel %u != 0", blockLevel);
+					const uint32_t rva = imageSectionStream.ConvertSectionOffsetToRVA(data.S_GPROC32_ID.section, data.S_GPROC32_ID.offset);
+					Printf(blockLevel, "S_GPROC32_ID Function '%s' | RVA 0x%X\n", data.S_GPROC32_ID.name, rva);
+					blockLevel++;
 				}
 				else
 				{
-					if (blockLevel != 0)
+					// We only care about records inside functions.
+					if (blockLevel > 0)
 					{
-						PDB_ASSERT(false, "Unhandled record kind 0x%X with block identation %u\n", record->header.kind, blockLevel);
+						PDB_ASSERT(false, "Unhandled record kind 0x%X with block level %u\n", record->header.kind, blockLevel);
 					}
 				}
 
-				if (rva == 0u)
-				{
-					return;
-				}
+				recordCount++;
 
-				PDB_ASSERT(blockLevel == 0, "BlockLevel %u != 0", blockLevel);
-
-				Printf(blockLevel, "Function: '%s' | RVA 0x%X\n", name, rva);
-
-				if (kind != SymbolRecordKind::S_TRAMPOLINE)
-				{
-					blockLevel++;
-				}
-
-				functionSymbols.push_back(FunctionSymbol{ name, rva, size, nullptr });
-				seenFunctionRVAs.emplace(rva);
 			});
 		}
 
-		scope.Done(modules.GetLength());
+		scope.Done(recordCount);
 	}
-
-	// we don't need to touch global symbols in this case.
-	// most of the data we need can be obtained from the module symbol streams, and the global symbol stream only offers data symbols on top of that, which we are not interested in.
-	// however, there can still be public function symbols we haven't seen yet in any of the modules, especially for PDBs that don't provide module-specific information.
-
-	// read public symbols
-	TimedScope publicScope("Reading public symbol stream");
-	const PDB::PublicSymbolStream publicSymbolStream = dbiStream.CreatePublicSymbolStream(rawPdbFile);
-	publicScope.Done();
-	{
-		TimedScope scope("Storing public function symbols");
-
-		const PDB::ArrayView<PDB::HashRecord> hashRecords = publicSymbolStream.GetRecords();
-		const size_t count = hashRecords.GetLength();
-
-		for (const PDB::HashRecord& hashRecord : hashRecords)
-		{
-			const PDB::CodeView::DBI::Record* record = publicSymbolStream.GetRecord(symbolRecordStream, hashRecord);
-			if ((PDB_AS_UNDERLYING(record->data.S_PUB32.flags) & PDB_AS_UNDERLYING(PDB::CodeView::DBI::PublicSymbolFlags::Function)) == 0u)
-			{
-				// ignore everything that is not a function
-				continue;
-			}
-
-			const uint32_t rva = imageSectionStream.ConvertSectionOffsetToRVA(record->data.S_PUB32.section, record->data.S_PUB32.offset);
-			if (rva == 0u)
-			{
-				// certain symbols (e.g. control-flow guard symbols) don't have a valid RVA, ignore those
-				continue;
-			}
-
-			// check whether we already know this symbol from one of the module streams
-			const auto it = seenFunctionRVAs.find(rva);
-			if (it != seenFunctionRVAs.end())
-			{
-				// we know this symbol already, ignore it
-				continue;
-			}
-
-			// this is a new function symbol, so store it.
-			// note that we don't know its size yet.
-			functionSymbols.push_back(FunctionSymbol{ record->data.S_PUB32.name, rva, 0u, nullptr});
-		}
-
-		scope.Done(count);
-	}
-
-
-	// we still need to find the size of the public function symbols.
-	// this can be deduced by sorting the symbols by their RVA, and then computing the distance between the current and the next symbol.
-	// this works since functions are always mapped to executable pages, so they aren't interleaved by any data symbols.
-	TimedScope sortScope("std::sort function symbols");
-	std::sort(functionSymbols.begin(), functionSymbols.end(), [](const FunctionSymbol& lhs, const FunctionSymbol& rhs)
-	{
-		return lhs.rva < rhs.rva;
-	});
-	sortScope.Done();
-
-	const size_t symbolCount = functionSymbols.size();
-	if (symbolCount != 0u)
-	{
-		TimedScope computeScope("Computing function symbol sizes");
-
-		size_t foundCount = 0u;
-
-		// we have at least 1 symbol.
-		// compute missing symbol sizes by computing the distance from this symbol to the next.
-		// note that this includes "int 3" padding after the end of a function. if you don't want that, but the actual number of bytes of
-		// the function's code, your best bet is to use a disassembler instead.
-		for (size_t i = 0u; i < symbolCount - 1u; ++i)
-		{
-			FunctionSymbol& currentSymbol = functionSymbols[i];
-			if (currentSymbol.size != 0u)
-			{
-				// the symbol's size is already known
-				continue;
-			}
-
-			const FunctionSymbol& nextSymbol = functionSymbols[i + 1u];
-			const size_t size = nextSymbol.rva - currentSymbol.rva;
-			(void)size; // unused
-			++foundCount;
-		}
-
-		// we know have the sizes of all symbols, except the last.
-		// this can be found by going through the contributions, if needed.
-		FunctionSymbol& lastSymbol = functionSymbols[symbolCount - 1u];
-		if (lastSymbol.size != 0u)
-		{
-			// bad luck, we can't deduce the last symbol's size, so have to consult the contributions instead.
-			// we do a linear search in this case to keep the code simple.
-			const PDB::SectionContributionStream sectionContributionStream = dbiStream.CreateSectionContributionStream(rawPdbFile);
-			const PDB::ArrayView<PDB::DBI::SectionContribution> sectionContributions = sectionContributionStream.GetContributions();
-			for (const PDB::DBI::SectionContribution& contribution : sectionContributions)
-			{
-				const uint32_t rva = imageSectionStream.ConvertSectionOffsetToRVA(contribution.section, contribution.offset);
-				if (rva == 0u)
-				{
-					printf("Contribution has invalid RVA\n");
-					continue;
-				}
-
-				if (rva == lastSymbol.rva)
-				{
-					lastSymbol.size = contribution.size;
-					break;
-				}
-				
-				if (rva > lastSymbol.rva)
-				{
-					// should have found the contribution by now
-					printf("Unknown contribution for symbol %s at RVA 0x%X", lastSymbol.name.c_str(), lastSymbol.rva);
-					break;
-				}
-			}
-		}
-
-		computeScope.Done(foundCount);
-	}
-
-	total.Done(functionSymbols.size());
 }
