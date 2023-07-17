7f185799e601d71030042f87f65eaaeef165bff9
MolecularMatters <61518974+MolecularMatters@users.noreply.github.com>
2023-07-17 12:52:12 +0200

    Removed UTF-8 characters/umlauts

--- src/Foundation/PDB_Forward.h
+++ src/Foundation/PDB_Forward.h
@@ -4,6 +4,6 @@
 #pragma once
 
 
-// See Jonathan Müller's blog for replacing std::move and std::forward:
+// See Jonathan Mueller's blog for replacing std::move and std::forward:
 // https://foonathan.net/2021/09/move-forward/
 #define PDB_FORWARD(...)				static_cast<decltype(__VA_ARGS__)&&>(__VA_ARGS__)
