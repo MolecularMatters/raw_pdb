7631e9bc12b7af4acfe5c371fcc413590af5f645
Luke Kasz <me@lukekasz.com>
2023-08-22 18:40:00 +0200

    Handle PDB::CodeView::TPI::TypeIndexKind::T_*REAL80 in GetTypeName

--- src/Examples/ExampleTypes.cpp
+++ src/Examples/ExampleTypes.cpp
@@ -187,6 +187,12 @@ static const char* GetTypeName(const TypeTable& typeTable, uint32_t typeIndex, u
 		case PDB::CodeView::TPI::TypeIndexKind::T_64PREAL64:
 		case PDB::CodeView::TPI::TypeIndexKind::T_PREAL64:
 			return "PDOUBLE";
+		case PDB::CodeView::TPI::TypeIndexKind::T_REAL80:
+			return "REAL80";
+		case PDB::CodeView::TPI::TypeIndexKind::T_32PREAL80:
+		case PDB::CodeView::TPI::TypeIndexKind::T_64PREAL80:
+		case PDB::CodeView::TPI::TypeIndexKind::T_PREAL80:
+			return "PREAL80";
 		case PDB::CodeView::TPI::TypeIndexKind::T_QUAD:
 			return "LONGLONG";
 		case PDB::CodeView::TPI::TypeIndexKind::T_32PQUAD:
